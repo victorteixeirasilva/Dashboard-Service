c1509e6ad9762bb2015fac01af253bec488effaa
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-06-13 19:09:54 -0300

    Desenvolver assinatura dos end-points a serem consumidos, objectives-service

--- dashboard/src/main/java/tech/inovasoft/inevolving/ms/dashboard/service/client/objective/ObjectiveServiceClient.java
+++ dashboard/src/main/java/tech/inovasoft/inevolving/ms/dashboard/service/client/objective/ObjectiveServiceClient.java
@@ -2,6 +2,7 @@ package tech.inovasoft.inevolving.ms.dashboard.service.client.objective;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import tech.inovasoft.inevolving.ms.dashboard.service.client.objective.dto.Objective;
 
@@ -12,21 +13,25 @@ import java.util.UUID;
         name = "objectives-service",
         url = "http://localhost:8081/ms/objectives"
 )
-public interface ObjectiveServiceClient {
+public interface ObjectiveServiceClient { //TODO: Desenvolver Teste de Integração.
 
+    @GetMapping("/{idObjective}/{idUser}")
     ResponseEntity<Objective> getObjectiveById(
             @PathVariable UUID idObjective,
             @PathVariable UUID idUser
     );
 
+    @GetMapping("/user/{idUser}")
     ResponseEntity<List<Objective>>  getObjectivesByIdUser(
             @PathVariable UUID idUser
     );
 
+    @GetMapping("/status/todo/user/{idUser}")
     ResponseEntity<List<Objective>> getObjectivesByIdUserToDo(
             @PathVariable UUID idUser
     );
 
+    @GetMapping("/status/done/user/{idUser}")
     ResponseEntity<List<Objective>> getObjectivesByIdUserDone(
             @PathVariable UUID idUser
     );
--- dashboard/src/main/java/tech/inovasoft/inevolving/ms/dashboard/service/client/task/TaskServiceClient.java
+++ dashboard/src/main/java/tech/inovasoft/inevolving/ms/dashboard/service/client/task/TaskServiceClient.java
@@ -1,5 +1,5 @@
 package tech.inovasoft.inevolving.ms.dashboard.service.client.task;
 
-public interface TaskServiceClient {
+public interface TaskServiceClient { //TODO: Desenvolver Teste de Integração.
     // TODO: Desenvolver assinatura dos end-points a serem consumidos
 }
