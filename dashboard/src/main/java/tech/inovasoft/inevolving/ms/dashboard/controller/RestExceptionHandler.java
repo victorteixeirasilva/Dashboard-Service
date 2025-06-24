bcfc10ec67dccb94c3d2979df0316c994d36640f
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-06-24 13:27:56 -0300

    Desenvolvendo Observabilidade de Error via Email

--- dashboard/src/main/java/tech/inovasoft/inevolving/ms/dashboard/controller/RestExceptionHandler.java
+++ dashboard/src/main/java/tech/inovasoft/inevolving/ms/dashboard/controller/RestExceptionHandler.java
@@ -1,5 +1,6 @@
 package tech.inovasoft.inevolving.ms.dashboard.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -7,12 +8,14 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import tech.inovasoft.inevolving.ms.dashboard.domain.dto.response.ExceptionResponse;
 import tech.inovasoft.inevolving.ms.dashboard.domain.exception.ExternalServiceErrorException;
 
-
+@Slf4j
 @ControllerAdvice
 public class RestExceptionHandler {
 
     @ExceptionHandler(ExternalServiceErrorException.class)
-    public ResponseEntity handleDataBaseException(ExternalServiceErrorException exception) {
+    public ResponseEntity<ExceptionResponse> handleDataBaseException(ExternalServiceErrorException exception) {
+        log.error("ERROR: {} - {}", exception.getClass().getSimpleName(), exception.getMessage());
+
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(new ExceptionResponse(
---
+++ dashboard/src/main/java/tech/inovasoft/inevolving/ms/dashboard/service/client/email_service/dto/EmailServiceException.java
@@ -0,0 +1,7 @@
+package tech.inovasoft.inevolving.ms.dashboard.service.client.email_service.dto;
+
+public class EmailServiceException extends RuntimeException {
+    public EmailServiceException(String e) {
+        super(e);
+    }
+}
